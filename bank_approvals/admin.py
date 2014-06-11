b62dce2132d98bc54557b0a8cbd500089590f9f7
vasughatole <vasughatole@gmail.com>
2014-06-11 14:00:26 +0530

    Email support for sending to banks added

--- bank_approvals/admin.py
+++ bank_approvals/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from bank_approvals.models import BankChoice
 import xlwt, datetime
 from signup.models import Company, MerchantBankApproval, Bank
+from django.core.mail import send_mail
+#from django.core.management.base import NoArgsCommand
+from django.core.mail.message import EmailMessage
 
 def unapproved_users(modeladmin,request,bank_obj,choiceList): 
 		unapp_user = MerchantBankApproval.objects.filter(status = "P")
@@ -69,12 +72,19 @@ def unapproved_users(modeladmin,request,bank_obj,choiceList):
 			colVal=0
 			rowVal= rowVal +1
 			####################header stops, body starts#####################3
-
+			workbook.save("Files/"+bank_obj.bank+".xlsx")
+			#send_mail('mail for bank approval', 'agar aaya to code chala.', 'vasughatole@gmail.com', ['utkarsh.dixit11@gmail.com'], fail_silently=False,)
+		#	class Command(NoArgsCommand):
+				#def handle_noargs(self, **options):
+			emailtemp=bank_obj.email
+			email = EmailMessage('subject rdydhhof the mail ', 'body of the mail', 'vasughatole@gmail.com', [''+emailtemp])
+			email.attach_file("Files/"+bank_obj.bank+".xlsx")
+			email.send()
 			for obj in unapp_user:
 				obj.status= "ES"
 				obj.date_mailed_on=datetime.datetime.now()
 				obj.save()
-			workbook.save("Files/"+bank_obj.bank+".xlsx")
+			
 			return unapp_user
 #################################################
 
